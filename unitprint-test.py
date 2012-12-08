ededc6ea69ca71114db8170a1567ff5f9a77ee7c
Martin Ueding <dev@martin-ueding.de>
2012-12-08 12:56:28 +0100

    Implement

--- unitprint-test.py
+++ unitprint-test.py
@@ -4,9 +4,22 @@
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
 import unittest
+import unitprint
 
 __docformat__ = "restructuredtext en"
 
 class UnitprintTest(unittest.TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+    def setUp(self):
+        unitprint.digits = 3
+
+    def test_number(self):
+        self.assertEqual(unitprint.format(1.23), "1.23e+00")
+
+    def test_small_number(self):
+        self.assertEqual(unitprint.format(0.123), "1.23e-01")
+
+    def test_error(self):
+        self.assertEqual(unitprint.format(1.23, 0.123), "(12.30 ± 1.23)e-01 (1.0e-01)")
+
+    def test_small_error(self):
+        self.assertEqual(unitprint.format(0.123, 0.123), "(1.23 ± 1.23)e-01 (1.0)")
--- unitprint.py
+++ unitprint.py
@@ -3,28 +3,72 @@
 
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
-import argparse
+import math
 
 __docformat__ = "restructuredtext en"
 
-def main():
-    options = _parse_args()
+digits = 3
+"""
+Number of digits to print. This can be overwritten.
+"""
 
-
-def _parse_args():
+def format(value, error=None, unit=None, lit=None):
     """
-    Parses the command line arguments.
+    Formats the given value and error in a human readable form. If an error is
+    supplied, it will calculate the relative error. If a literature value is
+    given, the deviation from the canonical value is calculated and the error
+    is given as a ratio and in the number of standard deviations.
 
-    :return: Namespace with arguments.
-    :rtype: Namespace
+    :param value: Value itself
+    :type value: float
+    :param error: Error of the value
+    :type error: None or float
+    :param unit: Physical unit
+    :type unit: None or str
+    :param lit: Canonical value
+    :type lit: None or float
+    :return: Formatted output
+    :rtype: str
     """
-    parser = argparse.ArgumentParser(description="")
-    #parser.add_argument("args", metavar="N", type=str, nargs="*", help="Positional arguments.")
-    #parser.add_argument("", dest="", type="", default=, help=)
-    #parser.add_argument("--version", action="version", version="<the version>")
 
-    return parser.parse_args()
+    parts = []
 
+    if error is None:
+        format_string = "{0:."+str(digits-1)+"e}"
+        parts.append(format_string.format(value))
+    else:
+        value_log = int(math.floor(math.log(value, 10)))
+        error_log = int(math.floor(math.log(error, 10)))
 
-if __name__ == "__main__":
-    main()
+        difference = value_log - error_log
+        exp = 0
+
+        if difference >= 0:
+            value_dis = value * 10**(difference - value_log)
+            error_dis = error * 10**(- error_log)
+            exp = error_log
+        else:
+            value_dis = value * 10**(- value_log)
+            error_dis = error * 10**(difference - error_log)
+            exp = value_log
+
+        format_string = "({0:."+str(digits-1)+"f} ± {1:."+str(digits-1)+"f})e{2:+02d}"
+        parts.append(format_string.format(value_dis, error_dis, exp))
+
+    if unit is not None:
+        parts.append(unit)
+
+    if error is not None:
+        parts.append("({0:.0%})".format(error/value))
+
+    if lit is not None:
+        parts.append("[{0:.0%}, {1:.1f}σ]".format((value-lit)/lit, (value-lit)/error))
+
+    return ' '.join(parts)
+
+if __name__ == '__main__':
+    print format(1.23, 1.23)
+    print format(12.3, 1.23)
+    print format(790e-9, 10e-9, "m")
+    print format(12.3, 1.23, lit=14.0)
+    print format(12.3, 1.23, "V", lit=10.0)
